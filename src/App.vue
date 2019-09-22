91ec7f3c42734c77a277c8bfddda31dc782a5a50
declan baldwin <dbaldwin4@boxfusionconsulting.com>
2019-09-22 11:42:03 +0100

    added people list and search functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,161 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Suade Front End Challenge 2019
+                    </h1>
+                    <h2 class="subtitle">
+                        Declan Baldwin
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="search-section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-half">
+                        <h4 class="title is-4">Select a person for details.</h4>
+                        <input
+                            class="input"
+                            type="text"
+                            placeholder="Search for a person here."
+                            v-model="searchText"
+                        />
+                        <button class="button is-success" @click="searchPeople()">
+                          Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="people-section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column">
+                        <div class="list is-hoverable">
+                            <a
+                                class="list-item"
+                                v-for="person in visiblePeople"
+                                v-bind:key="person.id"
+                                @click="selectedPerson = person"
+                            >
+                                {{ person.name }}
+                            </a>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <selected-person
+                            :person="selectedPerson"
+                        ></selected-person>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pagination">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-half">
+                        <nav
+                            class="pagination"
+                            role="navigation"
+                            aria-label="pagination"
+                        >
+                            <button
+                                class="pagination-previous"
+                                type="button"
+                                title="This is the first page"
+                                @click="updatePage(currentPage - 1)"
+                                :disabled="currentPage == 0"
+                            >
+                                Previous
+                            </button>
+                            <button
+                                class="pagination-next"
+                                type="button"
+                                @click="updatePage(currentPage + 1)"
+                                :disabled="currentPage == numberOfPages - 1"
+                            >
+                                Next page
+                            </button>
+                            <ul class="pagination-list">
+                                <li
+                                    v-for="(pageNumber, index) in numberOfPages"
+                                    v-bind:key="index"
+                                >
+                                    <a
+                                        class="pagination-link"
+                                        :class="{
+                                            'is-current': index == currentPage
+                                        }"
+                                        @click="updatePage(index)"
+                                        >{{ pageNumber }}</a
+                                    >
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectedPerson from "./components/SelectedPerson.vue";
+import peopleData from "./assets/people.json";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components: {
+        SelectedPerson
+    },
+    data() {
+        return {
+            people: peopleData,
+            selectedPerson: {},
+            currentPage: 0,
+            pageSize: 10,
+            visiblePeople: [],
+            searchText: '',
+            filterPeople: peopleData
+        };
+    },
+    methods: {
+        searchPeople() {
+            var $this = this;
+            this.filterPeople = this.people.filter(function(person) {
+                return (
+                    person.name
+                        .toLowerCase()
+                        .indexOf($this.searchText.toLowerCase()) !== -1
+                );
+            });
+            this.updatePage(0);
+
+        },
+        updateVisiblePeople() {
+            this.visiblePeople = this.filterPeople.slice(
+                this.currentPage * this.pageSize,
+                this.currentPage * this.pageSize + this.pageSize
+            );
+        },
+        updatePage(pageNumber) {
+            this.currentPage = pageNumber;
+            this.updateVisiblePeople();
+        }
+    },
+    computed: {
+        numberOfPages() {
+            return Math.ceil(this.filterPeople.length / this.pageSize);
+        }
+    },
+    mounted() {
+        this.updateVisiblePeople();
+    }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/SelectedPerson.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card" v-show="person.name">
+        <div class="card-content">
+            <p class="title is-4">{{ person.name }}</p>
+            <p>Age: {{ person.age }}</p>
+            <p>Eye Colour: {{ person.eyeColor }}</p>
+            <p>Gender: {{ person.gender }}</p>
+            <p>Location</p>
+            <p class="margin-left">Latitude: {{ person.location.latitude }}</p>
+            <p class="margin-left">Longitude: {{ person.location.longitude }}</p>
+            <p>Preferences</p>
+            <p class="margin-left">Pet: {{ person.preferences.pet }}</p>
+            <p class="margin-left">Fruit: {{ person.preferences.fruit }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SelectedPerson",
+    props: {
+        person: {}
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.margin-left {
+    margin-left: 1rem;
+}
+</style>
